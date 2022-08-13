a094edcb27d7b7f5eaa1a34e1db0bbeb5b20711e
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-13 02:24:47 -0700

    updated changes

--- home/home.js
+++ home/home.js
@@ -1,16 +1,65 @@
+let linksDiv = document.getElementById('navLinksResponsive')
+
 openNavDiv = () => {
-    let d = document.getElementById('navbar')
-    let a = document.getElementById('open_nav_div')
-    a.className = " showDiv"
-    d.className += " navHeigthIncrease"
+    let a = document.getElementById('navDiv')
+    a.className = " showNavDiv"
+
+
+    linksDiv.className = " showResponsiveLinks"
 }
 
 
 closeNavDiv = () => {
-    let b = document.getElementById('open_nav_div')
-    b.className += " hideDiv"
+    let b = document.getElementById('navDiv')
+    b.className = " hideNavDiv"
 
-    setTimeout(() => {
-        b.className = " none"
-    }, 550)
+
+    linksDiv.className = " hideResponsiveLinks"
+
+
 }
+
+
+
+
+
+
+
+let button1Clicked = false
+
+myToggle = () => {
+    let a = document.getElementById('navIcon')
+    let b = document.getElementById('toggleButton')
+
+
+    if (!button1Clicked) {
+        button1Clicked = true
+
+        setTimeout(() => {
+            b.innerHTML = '<i id="navIcon" class="fa fa-times" aria-hidden="true"></i>'
+        }, 150)
+        a.className += " navMenuIcon"
+        openNavDiv()
+        return;
+    }
+
+
+    if (button1Clicked) {
+        button1Clicked = false
+
+        setTimeout(() => {
+            b.innerHTML = '<i id="navIcon"class="fa fa-bars" aria-hidden="true"></i>'
+        }, 150)
+        a.className += " navCloseIcon"
+        closeNavDiv()
+        return;
+    }
+}
+
+
+
+
+
+
+// <i class="fa-solid fa-bars"></i>
+// <i class="fa-solid fa-xmark"></i>
