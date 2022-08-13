aff8a4d2882d85069f977eba1551f28509d22ba3
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-13 11:30:48 -0700

    updated changes

--- home/home.js
+++ home/home.js
@@ -20,10 +20,6 @@ closeNavDiv = () => {
 
 
 
-
-
-
-
 let button1Clicked = false
 
 myToggle = () => {
@@ -59,6 +55,17 @@ myToggle = () => {
 
 
 
+var myIndex = 0;
+carousel();
 
-// <i class="fa-solid fa-bars"></i>
-// <i class="fa-solid fa-xmark"></i>
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
