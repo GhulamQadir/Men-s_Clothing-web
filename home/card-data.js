7771304d46b76892d99170b95b69451c5143852d
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-14 14:35:32 -0700

    updated changes

--- home/card-data.js
+++ home/card-data.js
@@ -1,31 +1,33 @@
 let cardsMainDiv = document.getElementById('cards_main_div')
 
 let cardsData = [{
-    name: "Card 1",
-    image: "https://cdn.shopify.com/s/files/1/0144/7827/8739/products/DSC00131.jpg?v=1645467411"
+    name: "English Zip Polo Twinset - Black/Red",
+    image: "https://cdn.shopify.com/s/files/1/0144/7827/8739/products/105A7099copy_2000x.jpg?v=1646229301"
 },
 {
-    name: "Card 2",
-    image: "https://s.alicdn.com/@sc04/kf/H052b399dbf5b431e9bb2b857fb4c59f8f.jpg_960x960.jpg"
+    name: "Men's Red Polo Shirt",
+    image: "https://cdn.edenrobe.com/media/catalog/product/cache/27450b6fb6f8a26a4934fc76a933263c/0/n/0n9a1811-emtps21-022.jpg"
 },
 {
-    name: "Card 3",
-    image: "https://cdn.shopify.com/s/files/1/0518/0581/4952/products/1_3bd3bc81-2af9-4d90-a7b0-32e553a3cc41_590x.jpg?v=1623069035"
-},
-{
-    name: "Card 4",
+    name: "Men's White & Black Graphic Tee",
     image: "https://cdn.edenrobe.com/media/catalog/product/0/n/0n9a1748-emtgt21-008.jpg"
 },
 {
-    name: "Card 5",
-    image: "https://cdn.shopify.com/s/files/1/0144/7827/8739/products/AY5A2899_2000x.jpg?v=1646229908"
+    name: "Men's Black Chino Pant",
+    image: "https://cdn.edenrobe.com/media/catalog/product/0/n/0n9a2599-embcp22-007_1.jpg"
+},
+{
+    name: "Skull Head Sneakers",
+    image: "https://cdn.shopify.com/s/files/1/0518/0581/4952/products/1_3bd3bc81-2af9-4d90-a7b0-32e553a3cc41_590x.jpg?v=1623069035"
 }
 ]
 
 for (let i = 0; i < cardsData.length; i++) {
-    cardsMainDiv.innerHTML += `<div class="cards">
-    <img class="cards_images"
+    cardsMainDiv.innerHTML += `<div class="card">
+    <button class="cardbutton">hello</button>
+    <img class="card_image"
         src="${cardsData[i].image}">
+        <div class="my">hello</div>
     <p>${cardsData[i].name}</p>
 </div>`
 }
