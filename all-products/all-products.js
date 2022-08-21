25d4a90315b4d12162a35be47a49fa5751ee144d
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-08-21 13:18:21 -0700

    updated changes

---
+++ all-products/all-products.js
@@ -0,0 +1,52 @@
+let navDiv = document.getElementById('navDiv')
+let responsiveNavLinksDivMain = document.getElementById('responsiveNavLinksDivMain')
+
+openNavDiv = () => {
+
+    responsiveNavLinksDivMain.className = " showResponsiveNavLinksDiv"
+
+    navDiv.className = " showNavDiv"
+
+
+}
+
+
+closeNavDiv = () => {
+    navDiv.className = " hideNavDiv"
+    responsiveNavLinksDivMain.className += " hideResponsiveNavLinksDiv"
+
+
+}
+
+
+
+let button1Clicked = false
+
+myToggle = () => {
+    let a = document.getElementById('navIcon')
+    let b = document.getElementById('toggleButton')
+
+
+    if (!button1Clicked) {
+        button1Clicked = true
+
+        setTimeout(() => {
+            b.innerHTML = '<i id="navIcon" class="fa fa-times" aria-hidden="true"></i>'
+        }, 150)
+        a.className += " navMenuIcon"
+        openNavDiv()
+        return;
+    }
+
+
+    if (button1Clicked) {
+        button1Clicked = false
+
+        setTimeout(() => {
+            b.innerHTML = '<i id="navIcon"class="fa fa-bars" aria-hidden="true"></i>'
+        }, 150)
+        a.className += " navCloseIcon"
+        closeNavDiv()
+        return;
+    }
+}
