c7692ecf44c12d5c06159dad57080489a640aa05
GhulamQadir <hafizghulamqadir.2005@gmail.com>
2022-09-30 14:00:14 -0700

    increasing product quantity and updating total amount

--- cart/cart.js
+++ cart/cart.js
@@ -13,14 +13,31 @@ renderProductsOnCart = () => {
         
         <p class="prod-name">${cartProducts[i].name}</p>
         <p class="prod-price">RS: ${cartProducts[i].price}</p>
-        <p>Quantity: ${cartProducts[i].quantity}</p>
-        <button onclick="increaseQuanity()" class="increase_quantity">+</button>
+        <p>Quantity: <span>${cartProducts[i].quantity}<span></p>
+        <button onclick="increaseQuanity(this)" class="increase_quantity">+</button>
         </div>`
     }
 }
 renderProductsOnCart()
 
-increaseQuanity = () => {
+increaseQuanity = (e) => {
+    let initialValue = parseInt(e.parentNode.childNodes[7].childNodes[1].innerHTML)
+    e.parentNode.childNodes[7].childNodes[1].innerHTML = initialValue + 1
+
+
+
+    let productForIncrease = e.parentNode.childNodes[3].innerHTML
+    for (let i = 0; i < cartProducts.length; i++) {
+        if (cartProducts[i].name === productForIncrease) {
+            cartProducts[i].quantity += 1
+            localStorage.setItem("cart", JSON.stringify(cartProducts))
+            console.log(cartProducts[i])
+
+        }
+    }
+    getTotalAmount()
+
+
 }
 
 
@@ -29,14 +46,14 @@ getTotalAmount = () => {
     let amountsData = []
     let totalAmount = document.getElementById('total_amount')
     for (let i = 0; i < cartProducts.length; i++) {
-        amountsData.push(cartProducts[i].price)
+        amountsData.push(cartProducts[i].price * cartProducts[i].quantity)
     }
 
     let total = amountsData.reduce((total, currentVal) => {
         return total += currentVal
     })
 
-    totalAmount.innerHTML += total
+    totalAmount.innerHTML = `Total: ${total}`
     console.log(total)
 }
 
